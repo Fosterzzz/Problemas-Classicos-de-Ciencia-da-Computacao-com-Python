0184564cd9ca9a5aae81d9c4c44e20bc3ff43186
JoaoViana1 <vianajoaoguilherme1@gmail.com>
2022-03-17 15:06:16 -0300

    first commit

---
+++ Cap1/fib5.py
@@ -0,0 +1,13 @@
+def fib5(n: int) -> int:
+    if n == 0: return n
+    last: int = 0
+    next: int = 1
+    print(f'{last} \n {next}')
+    for _ in range(1, n):
+        last, next = next, last + next
+        print(f'{last} || {next}')
+    return next
+
+
+if __name__ == "__main__":
+    fib5(10)
