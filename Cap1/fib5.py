e4321313a9145dbfbf95eff2ee64edd7e29974f8
JoaoViana1 <74773184+JoaoViana1@users.noreply.github.com>
2022-04-24 15:15:17 -0300

    Update fib5.py

--- Cap1/fib5.py
+++ Cap1/fib5.py
@@ -2,9 +2,10 @@ def fib5(n: int) -> int:
     if n == 0: return n
     last: int = 0
     next: int = 1
-    print(f'{last} \n {next}')
+    print(f'{last}')
     for _ in range(1, n):
         last, next = next, last + next
+        print(last)
     return next
 
 
